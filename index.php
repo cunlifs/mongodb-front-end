12b939035fc0bd5b7a89cd17babad4224e639bd1
affian <nnamdiaffia@gmail.com>
2021-10-29 10:33:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,31 +5,30 @@ require('vendor/autoload.php');
 require('connection.php');
 
 // Display title and description
-echo '<h2 class="ds-heading-1 ds-col-10">Company Info</h2>
-<p class="ds-col-10 ds-margin-b-2">Access information from our sample database that represents company data. You can also see some information
-on the <a href="products.php">products</a> that we sell, also pulled from the same databaase, but from a different table.</p>
+echo '<h2 class="ds-heading-1 ds-col-10">Chicago Airbnb Listing Info</h2>
+<p class="ds-col-10 ds-margin-b-2">Access information from our sample database that represents Airbnb listing data.</p>
 ';
 
 // Collect the data from various endpoints
 
-// Start with the employee information - we use the API endpoint /getAllEmployees
+// Start with the listing information - we use the API endpoint /findall
 $rperfclient = new GuzzleHttp\Client([ 'base_uri'=>$apiBaseUri]);
-$rperfresponse = $rperfclient->request('GET', 'getAllEmployees');
+$rperfresponse = $rperfclient->request('GET', 'findall');
 $content = $rperfresponse->getBody();
 $jsonContent = json_decode($content, false);
 if ($jsonContent->success == 1) {
     $people = $jsonContent->data;
 
     // Now we can render our page
-    echo '<h3 class="ds-heading-2 ds-col-10">Employees</h3>
-    <p class="ds-col-10 ds-margin-b-2">Here is a directory of all of our employees. Click on an employee number to get more information about that individual.</p>
+    echo '<h3 class="ds-heading-2 ds-col-10">Listings</h3>
+    <p class="ds-col-10 ds-margin-b-2">Here is a directory of all of our listings. Click on a listing to get more information about it.</p>
     ';
     // The function drawTable can be found in functions.php
-    drawTable($people);
+    drawTable($listings);
 
 // If we don't have access to the API or database, provide information
 } else {
-    echo '<div class="ds-margin-t-b-2">API call to look up all employees has failed.
+    echo '<div class="ds-margin-t-b-2">API call to look up all listings has failed.
     ' . print_r($jsonContent) . '
     </div>';
 }
