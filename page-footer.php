4c91320c2bacdeae4c22eca346f7f4a329c99f3e
affian <nnamdiaffia@gmail.com>
2021-10-29 10:19:58 +0100

    Update page-footer.php

--- page-footer.php
+++ page-footer.php
@@ -5,7 +5,7 @@
     <footer class="ds-sticky-footer ds-grid ds-bg-neutral-warm-2" style="width: 100%; height: 110px;" >
       <div class="ds-row">
         <h5 class="ds-offset-xs-1 ds-col-xs-10 ds-pad-t-2_5 ds-heading-5">
-          <a href="https://github.com/andrewlaidlaw/db2-front-end" class="ds-text-neutral-6 ds-font-weight-light">GitHub Repository</a> |
+          <a href="https://github.com/affian/mongodb-front-end" class="ds-text-neutral-6 ds-font-weight-light">GitHub Repository</a> |
           <a href="https://techzone.ibm.com" class="ds-text-neutral-6 ds-font-weight-light">IBM TechZone</a> |
           <a href="https://www.ibm.com/uk-en/it-infrastructure/power" class="ds-text-neutral-6 ds-font-weight-light">IBM Power</a>
         </h5>
