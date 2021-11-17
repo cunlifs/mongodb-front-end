823d9fdb2b609d8b7d8c7fae6991f93431a3de7f
affian <nnamdiaffia@gmail.com>
2021-11-17 14:43:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ echo '<h2 class="ds-heading-1 ds-col-10">Chicago Airbnb Listing Info</h2>
 
 // Collect the data from various endpoints
 
-// Start with the listing information - we use the API endpoint /findall
+// Start with the listing information - we use the API endpoint /findListings
 $rperfclient = new GuzzleHttp\Client([ 'base_uri'=>$apiBaseUri]);
-$rperfresponse = $rperfclient->request('GET', 'findall');
+$rperfresponse = $rperfclient->request('GET', 'findListings');
 $content = $rperfresponse->getBody();
 $jsonContent = json_decode($content, false);
 if ($jsonContent->success == 1) {
