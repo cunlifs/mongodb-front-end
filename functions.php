eb311e6b5db6f61b487c4c0452238e2b754c2ffa
affian <nnamdiaffia@gmail.com>
2021-11-17 10:49:49 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@ function drawLine($listing) {
     echo '<tr>';
     echo '<td><a href="listing.php?id=' . $listing['id'] . '">' . $listing['id'] . '</a></td>';
     echo '<td>' . $listing['host_id'] . '</td>';
-    echo '<td>' . $listing['accommodates'] . '</td>';
-    echo '<td>' . $listing['bedrooms'] . '</td>';
     echo '<td>' . $listing['beds'] . '</td>';
+    echo '<td>' . $listing['bedrooms'] . '</td>';
+    echo '<td>' . $listing['accommodates'] . '</td>';
     echo '</tr>
     ';
 }
@@ -17,7 +17,7 @@ function drawTable($listings) {
     echo '<div class="ds-table-container ds-col-8">
     <table class="ds-table ds-table-compact ds-striped ds-hover">
     ';
-    echo '<tr><th>Listing ID</th><th>Host ID</th><th>Accommodates</th><th>Bedrooms</th><th>Beds</th></tr>
+    echo '<tr><th>Listing ID</th><th>Host ID</th><th>Beds</th><th>Bedrooms</th><th>Accommodates</th></tr>
     ';
     // Iterate over the set of listings and create a row in the table
     foreach($listings as $listing) {
