18e4ecc3db757922522024513fcc025ffa76f4a2
affian <nnamdiaffia@gmail.com>
2021-11-18 10:36:21 +0000

    Update listing.php

--- listing.php
+++ listing.php
@@ -19,7 +19,7 @@ We have called against another API endpoint to pull this data from our database.
 
 // Start with the listing information - we use the API endpoint 
 $rperfclient = new GuzzleHttp\Client([ 'base_uri'=>$apiBaseUri]);
-$rperfresponse = $rperfclient->request('GET', 'findListings');
+$rperfresponse = $rperfclient->request('GET', 'findall?id=' . $_GET['id']);
 $content = $rperfresponse->getBody();
 $jsonContent = json_decode($content, false);
 if ($jsonContent->success == 1) {
