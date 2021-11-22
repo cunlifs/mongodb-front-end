8b3fe34f6655b7e6328b270c9a26ce7ff04b48c3
affian <nnamdiaffia@gmail.com>
2021-11-22 11:32:52 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -34,11 +34,21 @@ function renderListing($listing) {
     // within the front-end code to provide further insights and more detail to the end user.
     // We are not limited to using just the data that comes from our API calls.
 
-    // Create a nicely formatted version of the listing name, neighbourhood, property type and host's first name
+    // Create a nicely formatted version of the listing info
+    //property
     $propertyName = ucfirst(strtolower($listing->name));
-    $neighborhood = ucfirst(strtolower($listing->neighborhood));
+    $description = ucfirst(strtolower($listing->description));
+    $neighborhood = ucfirst(strtolower($listing->neighborhood_overview));
     $propertyType = ucfirst(strtolower($listing->property_type));
+    $bathrooms = ucfirst(strtolower($listing->bathrooms_text));
+    $bedrooms = ucfirst(strtolower($listing->bedrooms));
+    $beds = ucfirst(strtolower($listing->beds));
+    $price = ucfirst(strtolower($listing->price));
+    //host
     $hostName = ucfirst(strtolower($listing->host_name));
+    $hostSince = ucfirst(strtolower($listing->host_since));
+    $hostAbout = ucfirst(strtolower($listing->host_about));
+    $hostResponseRate = ucfirst(strtolower($listing->host_response_rate));
 
     // Here we present that information to the user
     echo '<div class="ds-row">
