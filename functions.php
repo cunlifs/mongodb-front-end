d791da1a59556cdf17cef6a7ba197a1483cd9529
affian <nnamdiaffia@gmail.com>
2021-11-22 11:01:19 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ function drawTable($listings) {
     echo '<div class="ds-table-container ds-col-8">
     <table class="ds-table ds-table-compact ds-striped ds-hover">
     ';
-    echo '<tr><th>ID</th><th>Property Name</th><th>Location</th><th>Host</th><th>Accommodates</th></tr>
+    echo '<tr><th>Listing ID</th><th>Property Name</th><th>Location</th><th>Host\'s Name</th><th>Accommodates</th></tr>
     ';
     // Iterate over the set of listings and create a row in the table
     foreach($listings as $listing) {
