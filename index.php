ae708de90c80642f28d270a46213458a3e51f57b
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2021-11-24 15:52:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,45 +4,43 @@ require_once('page-header.php');
 require('vendor/autoload.php');
 require('connection.php');
 
+// Include a way to return to the previous page
+echo '<h4 class="ds-heading-4 ds-margin-t-b-2"><a href="index.php">&lt back to complete listings</a></h4>
+';
+
 // Display title and description
-echo '<h2 class="ds-heading-1 ds-col-10">Chicago Airbnb Listing Info.</h2>
-<p class="ds-col-10 ds-margin-b-2">Access information from our sample database that represents Airbnb listing data.</p>
+echo '<div class="ds-row"><h2 class="ds-heading-1 ds-col-6">Listing Info</h2>
+<p class="ds-col-6 ds-margin-b-2">This information relates to the listing that you selected from the previous list.
+We have called against another API endpoint to pull this data from our database.</p>
+</div>
 ';
 
 // Collect the data from various endpoints
 
 // Start with the listing information - we use the API endpoint 
 $rperfclient = new GuzzleHttp\Client([ 'base_uri'=>$apiBaseUri]);
-//$rperfresponse = $rperfclient->request('GET', 'findall');
 $rperfresponse = $rperfclient->request('GET', 'findall?id=' . $_GET['id']);
 $content = $rperfresponse->getBody();
-$jsonContent = json_decode($content, false);
+$jsonContent = json_decode($content, true);
 $listing = $jsonContent[0];
 renderListing($listing);
-
-    // Now we can render our page
-echo '<h3 class="ds-heading-2 ds-col-10">Listings</h3>
-<p class="ds-col-10 ds-margin-b-2">Here is a directory of all of our listings. Click on a listing to get more information about it.</p>
-';
-drawTable($jsonContent);
-
-/*if ($jsonContent->success == 1) {
-    $listings = $jsonContent->data[];
-
-    // Now we can render our page
-    echo '<h3 class="ds-heading-2 ds-col-10">Listings</h3>
-    <p class="ds-col-10 ds-margin-b-2">Here is a directory of all of our listings. Click on a listing to get more information about it.</p>
-    ';
-    // The function drawTable can be found in functions.php
-    drawTable($listings);
-
-// If we don't have access to the API or database, provide information
+/*
+if ($jsonContent->success == 1) {
+    $listing = $jsonContent[0];
+    // Now we can render our page using that data
+    // The renderListing function can be found in functions.php
+    renderListing($listing);
+// If the lookup fails, provide information
 } else {
-    echo '<div class="ds-margin-t-b-2">API call to look up all listings has failed (index.php).
+    echo '<div class="ds-margin-t-b-2">API call to look up listing has failed.
     ' . print_r($jsonContent) . '
     </div>';
-}*/
-echo '<p class="ds-col-10 ds-margin-b-2">&nbsp</p>';
+} */
+
+// Include a way to return to the previous page
+echo '<br />
+<h4 class="ds-heading-4 ds-margin-t-b-2"><a href="index.php">&lt back to complete listings</a></h4>
+';
 
 // Add footer from template
 require_once('page-footer.php');
