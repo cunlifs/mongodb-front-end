2d1e1d19d58281ce3fcda2c322c82745f84c9597
Stu Cunliffe <s_cunliffe@uk.ibm.com>
2021-11-24 15:31:14 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,8 +6,9 @@
 if ($_ENV['MONGODB_API_SERVICE_NAME']) {
     $apiInstanceName = $_ENV['MONGODB_API_SERVICE_NAME'];
 } else {
-    $apiInstanceName = 'MONGO_APP_NODE';
+    $apiInstanceName = 'MONGO_APP_NODE_DEMO3';
 }
+// Was     $apiInstanceName = 'MONGO_APP_NODE';
 
 // Here we set the endpoint of our API instance
 $apiBaseUri = 'http://' . $_ENV[$apiInstanceName.'_SERVICE_HOST'] . ':' . $_ENV[$apiInstanceName.'_SERVICE_PORT'];
